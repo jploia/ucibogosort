ffae895d4b9c090cb84a8c8015e65a99c1409e5c
Julia Nguyen <juliatnguyen05@gmail.com>
2025-01-25 14:14:47 -0800

    add centered title text

--- garbogotchi/app/page.tsx
+++ garbogotchi/app/page.tsx
@@ -14,27 +14,29 @@ export default function Main() {
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
-        className="object-fill min-h-screen"
+        className="object-fill h-screen w-100%"
       >
-        <div className="flex-1 h-screen w-100% justify-center items-center">
-          <div className="flex justify-center flex-wrap">
-            <p className="font-cabin text-9xl">Garba</p>
-            <p className="font-jersey text-9xl">gotchi</p>
-          </div>
-          <p className="flex gap-[2rem] justify-center flex-wrap font-cabin text-xl">
-            Learn what being in a top 10 Cool School means.
-          </p>
-          <Link
-            className="flex justify-center flex-wrap font-cabin text-md pt-4"
-            href="/gameplay"
-          >
-            <motion.button
-              whileHover={{ scale: 1.1, backgroundColor: "#202AB7" }}
-              className="font-jersey text-white text-6xl bg-dark-blue px-3 py-1.25 rounded-3xl"
+        <div className="flex absolute h-[100vh] w-[100vw] justify-center items-center">
+          <div>
+            <div className="flex justify-center flex-wrap">
+              <p className="font-cabin text-8xl">Garba</p>
+              <p className="font-jersey text-8xl">gotchi</p>
+            </div>
+            <p className="flex gap-[2rem] justify-center flex-wrap font-cabin text-md">
+              Learn what being in a top 10 Cool School means.
+            </p>
+            <Link
+              className="flex justify-center flex-wrap font-cabin text-md pt-4"
+              href="/gameplay"
             >
-              Start
-            </motion.button>
-          </Link>
+              <motion.button
+                whileHover={{ scale: 1.1, backgroundColor: "#202AB7" }}
+                className="font-jersey text-white text-6xl bg-dark-blue px-3 py-1.25 rounded-2xl"
+              >
+                Start
+              </motion.button>
+            </Link>
+          </div>
         </div>
       </div>
     </>
