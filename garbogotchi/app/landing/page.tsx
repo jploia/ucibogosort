a5f6585ca910137ac6bf70e6c76785cd4077a607
Julia Nguyen <juliatnguyen05@gmail.com>
2025-01-25 09:42:13 -0800

    fix button hover

--- garbogotchi/app/landing/page.tsx
+++ garbogotchi/app/landing/page.tsx
@@ -6,18 +6,18 @@ import { motion } from "framer-motion";
 export default function LandingPage() {
   return (
     <>
-      <p className="font-cabin text-7xl">Garbagotchi</p>
-      <p className="font-cabin text-md">
-        Learn what being the #1 Cool School means.
-      </p>
-      <a className="font-jersey text-white text-3xl bg-neon-green px-3 py-1.5 rounded-3xl">
+      <div className="">
+        <p className="font-cabin text-7xl">Garbagotchi</p>
+        <p className="font-cabin text-md">
+          Learn what being the #1 Cool School means.
+        </p>
         <motion.button
-          whileHover={{ scale: 1.1, backgroundColor: "dark-green" }}
-          className="px-3 py-1.5 rounded-3xl"
+          whileHover={{ scale: 1.1, backgroundColor: "#37C218" }}
+          className="font-jersey text-white text-3xl bg-neon-green px-3 py-1.5 rounded-3xl"
         >
           Start
         </motion.button>
-      </a>
+      </div>
     </>
   );
 }
