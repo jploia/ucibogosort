7803d355b4c9220615e4dc65f894dbe263455a94
Julia Nguyen <juliatnguyen05@gmail.com>
2025-01-25 16:37:55 -0800

    add blue bg for gameplay

--- garbogotchi/app/gameplay/page.tsx
+++ garbogotchi/app/gameplay/page.tsx
@@ -3,14 +3,17 @@ import BinContainer from "@/app/components/BinContainer";
 export default function GameplayPage() {
   const binType = [
     {
+      name: "compost",
       src: "/compost.PNG",
       alt: "small green bin with banana peel logo on it",
     },
     {
+      name: "recycle",
       src: "/recycle.PNG",
       alt: "small blue bin with a recycling logo on it",
     },
     {
+      name: "trash",
       src: "/trash.PNG",
       alt: "small black bin with a trash wrapper logo on it",
     },
@@ -18,7 +21,9 @@ export default function GameplayPage() {
 
   return (
     <>
-      <BinContainer bins={binType} />
+      <div style={{ backgroundColor: "#8FB6F4" }}>
+        <BinContainer bins={binType} />
+      </div>
     </>
   );
 }
