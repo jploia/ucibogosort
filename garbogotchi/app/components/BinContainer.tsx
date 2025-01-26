f6e1fbfb1ed2033035f4e7e36f9a8a103d631b9d
Julia Nguyen <juliatnguyen05@gmail.com>
2025-01-25 16:40:01 -0800

    add api request

--- garbogotchi/app/components/BinContainer.tsx
+++ garbogotchi/app/components/BinContainer.tsx
@@ -1,7 +1,27 @@
+"use client";
+
 import BinProps from "@/app/components/BinProps";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function BinContainer(props: Readonly<{ bins: BinProps[] }>) {
+  async function getData() {
+    const url = "http://127.0.0.1:5000?get_prompts=10";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.error;
+    }
+  }
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <>
       <div className="flex gap-[2rem] min-h-screen justify-center items-center flex-wrap">
