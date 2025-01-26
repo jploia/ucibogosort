017814f9ed82f9b677de6ea04a5b8c87cc36ea33
Julia Nguyen <juliatnguyen05@gmail.com>
2025-01-25 16:45:51 -0800

    fix location of api call

--- garbogotchi/app/gameplay/page.tsx
+++ garbogotchi/app/gameplay/page.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import BinContainer from "@/app/components/BinContainer";
+import { useEffect, useState } from "react";
 
 export default function GameplayPage() {
   const binType = [
@@ -13,11 +16,28 @@ export default function GameplayPage() {
       alt: "small blue bin with a recycling logo on it",
     },
     {
-      name: "trash",
+      name: "landfill",
       src: "/trash.PNG",
       alt: "small black bin with a trash wrapper logo on it",
     },
   ];
+
+  async function getData() {
+    const url = "http://127.0.0.1:5000?get_prompts=10";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.error;
+    }
+  }
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <>
