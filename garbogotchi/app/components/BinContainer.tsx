bca1a1cb3053b9301cfd372ec3e65c2f5cb55e25
Julia Nguyen <juliatnguyen05@gmail.com>
2025-01-25 17:20:33 -0800

    add motion to bin

--- garbogotchi/app/components/BinContainer.tsx
+++ garbogotchi/app/components/BinContainer.tsx
@@ -1,5 +1,6 @@
 import BinProps from "@/app/components/BinProps";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 export default function BinContainer(props: Readonly<{ bins: BinProps[] }>) {
   return (
@@ -7,13 +8,15 @@ export default function BinContainer(props: Readonly<{ bins: BinProps[] }>) {
       <div className="flex gap-[2rem] min-h-screen justify-center items-center flex-wrap">
         {props.bins.map((bin, index) => {
           return (
-            <Image
-              key={index}
-              src={bin.src}
-              width={400}
-              height={400}
-              alt={bin.alt}
-            />
+            <motion.div whileHover={{ scale: 1.1 }}>
+              <Image
+                key={index}
+                src={bin.src}
+                width={400}
+                height={400}
+                alt={bin.alt}
+              />
+            </motion.div>
           );
         })}
       </div>
